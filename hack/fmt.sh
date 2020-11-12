65e2d4a3ae3f089cecd4a4db9153a7dc38b83a2d
zouyu <zouy.fnst@cn.fujitsu.com>
2020-11-12 09:17:02 +0800

    Update fmt.sh
    Signed-off-by: zouyu <zouy.fnst@cn.fujitsu.com>

--- hack/fmt.sh
+++ hack/fmt.sh
@@ -2,15 +2,15 @@
 
 set -u
 
+exitcode=0
+
 for file in $(find . -path ./.git -prune -o -type f | grep -E ".*\.go$")
 do
     result=$(./tools/gofmt -s -d $file)
     if [ "$result" != "" ];then
         echo "$result"
-        exit 1
+        exitcode=1
     fi
 done
 
-
-
-
+exit $exitcode
