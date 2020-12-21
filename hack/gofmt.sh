b7ce9072498c286c36616723f1897aa9be1080e3
zouyu <zouy.fnst@cn.fujitsu.com>
2020-12-21 09:15:56 +0800

    Update scrpit
    Signed-off-by: zouyu <zouy.fnst@cn.fujitsu.com>

--- hack/gofmt.sh
+++ hack/gofmt.sh
@@ -6,7 +6,7 @@ exitcode=0
 
 for file in $(find . -path ./.git -prune -o -type f | grep -E ".*\.go$")
 do
-    result=$(./tools/gofmt -s -d -w $file)
+    result=$(./tools/gofmt -s -d -w "$file")
     if [ "$result" != "" ];then
         echo "$result"
         exitcode=1
